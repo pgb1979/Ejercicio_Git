abb8baacae29e4d6a740d423cd47d2ae4aa8afa2
pgb1979 <p.gb@outlook.es>
2026-01-11 14:24:59 +0100

    Agregar funcionalidad de menu a la calculadora

--- Ejercicio_Git/Program.cs
+++ Ejercicio_Git/Program.cs
@@ -10,6 +10,33 @@ namespace Ejercicio_Git
     {
         static void Main(string[] args)
         {
+            double resultado = 0, num1 = 10, num2 = 20;
+
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+
+            Console.Write("Escoge una opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado = Suma(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                case 2:
+                    resultado = Resta(num1, num2);
+                    Console.WriteLine($"El resultado de la resta es: {resultado}");
+                    break;
+                case 3:
+                    resultado = Multiplicacion(num1, num2);
+                    Console.WriteLine($"El resultado de la multiplicación es: {resultado}");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
         }
 
         static double Suma(double num1, double num2)
